faf153d49316b87c8d4b5e546f448eb86758cce1
Francisco Sanchez <c0d3cisco@yahoo.com>
2023-03-29 16:17:03 -0500

    integrated choice with some profile code

--- js/choices.js
+++ js/choices.js
@@ -1,18 +1,18 @@
-'use strict';
+// 'use strict';
 
-let kidName;
-let animal;
-let color;
-let number;
+// let kidName;
+// let animal;
+// let color;
+// let number;
 
-function Profiles(kidName, color, animal, number) {
-  this.kidName = kidName;
-  this.color = color;
-  this.animal = animal;
-  this.number = number;
-  this.timesVideoWatched = 0;
-  this.timesPlayedPhysics = 0;
-}
+// function Profiles(kidName, color, animal, number) {
+//   this.kidName = kidName;
+//   this.color = color;
+//   this.animal = animal;
+//   this.number = number;
+//   this.timesVideoWatched = 0;
+//   this.timesPlayedPhysics = 0;
+// }
 
 // function startQuestions() {
 
@@ -99,76 +99,124 @@ function Profiles(kidName, color, animal, number) {
 
 
 
-  let profile = new Profiles(kidName, animal, color, number);
+// let profile = new Profiles(kidName, animal, color, number);
 
 
-  let profileStringify = JSON.stringify(profile);
-  localStorage.setItem(this.name, profileStringify);
+// let profileStringify = JSON.stringify(profile);
+// localStorage.setItem(this.name, profileStringify);
 
 
+//profile construct function
+function Profiles(name, color, animal, number){
+  this.name = name;
+  this.color = color;
+  this.animal = animal;
+  this.number = number;
+  this.timesVideoWatched = 0;
+  this.timesPlayedPhysics = 0;
 }
+
+//method called when a video is watched
+Profiles.prototype.timesVideoWatched = function() {
+  this.timesVideoWatched += 1;
+};
+
+// method called when game is played (maybe every time ball is thrown?)
+Profiles.prototype.timesPlayedPhysics = function() {
+  this.timesPlayedPhysics += 1;
+};
 
 
 const questions = [
-    {
-      title: 'What is your favorite animal?',
-      choices: ['jaguar', 'monkey', 'octopus'],
-    },
-    {
-      title: 'What is your favorite color?',
-      choices: ['red', 'blue', 'green'],
-    },
-    {
-      title: 'What is your favorite number?',
-      choices: ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
-    },
-  ];
-  
-  let currentQuestion = 0;
-  let selectedChoices = [];
-  
-  function startQuestions() {
-    const name = document.getElementById('nameInput').value;
-    localStorage.setItem('name', name);
-  
-    const questionsContainer = document.getElementById('questions');
-    questionsContainer.style.display = 'block';
-    showQuestion(currentQuestion);
-  }
-  
-  function showQuestion(index) {
-    const question = questions[index];
-    const container = document.getElementById('questions');
-    container.innerHTML = `<h2>${question.title}</h2>`;
-  
-    question.choices.forEach((choice) => {
-      const image = document.getElementById(choice).cloneNode(true);
-      image.style.display = 'inline-block'; // Show the image
-      image.onclick = () => {
-        selectedChoices.push(choice);
-        if (currentQuestion < questions.length - 1) {
-          currentQuestion++;
-          showQuestion(currentQuestion);
-        } else {
-          localStorage.setItem('choices', JSON.stringify(selectedChoices));
-          window.location.href = 'home.html';
+  {
+    title: 'What is your favorite animal?',
+    choices: ['jaguar', 'monkey', 'octopus'],
+  },
+  {
+    title: 'What is your favorite color?',
+    choices: ['red', 'blue', 'green'],
+  },
+  {
+    title: 'What is your favorite number?',
+    choices: ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
+  },
+];
+
+let currentQuestion = 0;
+let selectedChoices = [];
+// const profileArray = JSON.parse(localStorage.getItem('profileArray')) || [];
+function startQuestions() {
+  // const name = document.getElementById('nameInput').value;
+  // console.log(name);
+  // debugger;
+  // // localStorage.setItem('name', name); // testing for integrating profile array contruct
+
+  const questionsContainer = document.getElementById('questions');
+  questionsContainer.style.display = 'block';
+  showQuestion(currentQuestion, name);
+}
+
+function showQuestion(iQ,name) {
+  const question = questions[iQ];
+  const container = document.getElementById('questions');
+  container.innerHTML = `<h2>${question.title}</h2>`;
+
+  question.choices.forEach((choice) => {
+    const image = document.getElementById(choice).cloneNode(true);
+    image.style.display = 'inline-block'; // Show the image
+    image.onclick = () => {
+      selectedChoices.push(choice);
+      if (currentQuestion < questions.length - 1) {
+        currentQuestion++;
+        showQuestion(currentQuestion);
+      } else {
+        // localStorage.setItem('choices', JSON.stringify(selectedChoices));
+        // const name = localStorage.getItem('name');
+        const animal = selectedChoices[0];
+        const color = selectedChoices[1];
+        const number = selectedChoices[2];
+        debugger;
+        let index = parseInt(appState);
+        let name = document.getElementById('nameInput').value;
+        if(true){//appState === 'newUser'){ // captures new user if appState is new user
+          const profile = new Profiles(name, color, animal, number);
+          profileArray.push(profile);
+          console.log(profileArray);
+        }else if (index>= 0){
+          console.log(profileArray);
+          profileArray[index].name = name;
+          profileArray[index].color = color;
+          profileArray[index].animal = animal;
+          profileArray[index].number = number;
         }
-      };
-      container.appendChild(image);
-    });
-  }
+        debugger;
+        let profileArrayString = JSON.stringify(profileArray);
+        console.log(profileArrayString);
+        debugger;
+        localStorage.setItem('profileArray', profileArrayString);
+        window.location.href = 'home.html';
+
+        // let profile = new Profiles(name, animal, color, number);
+        // let profileStringify = JSON.stringify(profile);
+        // localStorage.setItem(name, profileStringify);
+      }
+    };
+    container.appendChild(image);
+  });
   
-  // Populate the home page with the user's choices
-  if (document.getElementById('name')) {
-    document.getElementById('name').innerText = localStorage.getItem('name');
-    const choices = JSON.parse(localStorage.getItem('choices'));
-    const choicesContainer = document.getElementById('choices');
-  
-    choices.forEach((choice) => {
-      const image = document.getElementById(choice).cloneNode(true);
-      image.style.display = 'inline-block'; // Show the image
-      choicesContainer.appendChild(image);
-    });
-  }
-  
-  
+}
+
+// Populate the home page with the user's choices
+if (document.getElementById('name')) {
+  document.getElementById('name').innerText = localStorage.getItem('name');
+  const choices = JSON.parse(localStorage.getItem('choices'));
+  const choicesContainer = document.getElementById('choices');
+
+  choices.forEach((choice) => {
+    const image = document.getElementById(choice).cloneNode(true);
+    image.style.display = 'inline-block'; // Show the image
+    choicesContainer.appendChild(image);
+  });
+}
+
+
